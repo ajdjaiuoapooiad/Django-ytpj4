87adcbc69e19ff2a36048cf44c7481923e2b3d3d
user <user@usernoMacBook-Air.local>
2024-10-09 14:36:00 +0900

    28ch channelModels ok🎉

---
+++ channel/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from taggit.managers import TaggableManager
+
+from django.conf import settings
+from core.models import user_directory_path
+
+User = settings.AUTH_USER_MODEL
+
+
+STATUS =  (
+    ("active", "Active"),
+    ("disable", "Disable"),
+   
+)
+
+
+
+class Channel(models.Model):
+    channel_art = models.ImageField(upload_to=user_directory_path,null=True, blank=True,default='images/channel-art.jpg')
+    image = models.ImageField(upload_to=user_directory_path, null=True, blank=True)
+    full_name = models.CharField(max_length=100) 
+    channel_name = models.CharField(max_length=100) 
+    description = models.TextField(null=True, blank=True)
+    keywords = TaggableManager()
+    joined = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=STATUS, max_length=100, default="active")
+    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True,related_name="channel")
+    subscribers = models.ManyToManyField(User,related_name="user_subs")
+
+    def __str__(self):
+        return self.channel_name
