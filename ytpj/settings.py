a3397cf48e122c654153ed0365b7a1621daf3bb8
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-10-09 10:41:54 +0900

    indexView ok

--- ytpj/settings.py
+++ ytpj/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -137,3 +138,12 @@ AUTH_USER_MODEL = "userauths.User"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+    # "site_title": "MovieX",
+    "site_header": "Youtube Studio",
+    "site_brand": "Think | Create | Inspire",
+    # "site_logo": "images/logo.jpg",
+    "copyright": "youtube.com",
+    # "order_with_respect_to": ["blog"]
+}
