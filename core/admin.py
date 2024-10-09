f1f3b9ca2c9701feeeb94de676dacf9f5d8a1a8a
user <user@usernoMacBook-Air.local>
2024-10-09 16:44:00 +0900

    commentModels ok

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,15 @@
+
 from django.contrib import admin
 
-from core.models import Video
+from core.models import Comment, Video
 from import_export.admin import ImportExportModelAdmin
 
 class VideoAdmin(ImportExportModelAdmin):
     pass
 
+class CommentAdmin(ImportExportModelAdmin):
+    list_display = ["user", "comment", "active", "video"]
+
 
 admin.site.register(Video,VideoAdmin)
+admin.site.register(Comment,CommentAdmin)
--- core/models.py
+++ core/models.py
@@ -27,3 +27,14 @@ class Video(models.Model):
 
     def __str__(self):
         return self.title
+    
+    
+class Comment(models.Model):
+    comment=models.CharField(max_length=1000)
+    user=models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    active=models.BooleanField(default=True)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE, related_name="comments")
+    date=models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.comment[:30]
