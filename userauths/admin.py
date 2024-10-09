ea0a9c1bd388cbc0ab77788e83537ed60f2b242e
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-10-09 10:47:20 +0900

    -25ch ok

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
 from userauths.models import User
+from import_export.admin import ImportExportModelAdmin
 
-admin.site.register(User)
+class UserAdmin(ImportExportModelAdmin):
+    pass
+
+admin.site.register(User,UserAdmin)
