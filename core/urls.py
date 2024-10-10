7ce6acf3886198dba4fbba3d859ffa99adc9478b
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-10-10 09:33:12 +0900

    30ch ok🍷

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,7 @@ from core import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("watch/<int:pk>/", views.videoDetail, name="video-detail"),
+    
+     # Saving Comment to db
+    path("ajax-save-comment/", views.ajax_save_comment, name="save-comment"),
 ]
