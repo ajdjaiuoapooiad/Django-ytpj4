ce177b00846f155ee39858ff7084ea97cdfefda6
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-10-12 16:21:12 +0900

    39ch ok community-comment🍷

--- channel/urls.py
+++ channel/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     path("<channel_name>/community/", views.channel_community, name="channel-community"),
     path("<channel_name>/community/<int:community_id>", views.channel_community_detail, name="channel-community-detail"),
 
+    # Create COmment URL
+    path("community/<int:community_id>/create-comment/", views.create_comment, name="community-create-comment"),
+
 
 
 
