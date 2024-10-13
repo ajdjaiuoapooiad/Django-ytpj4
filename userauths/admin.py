b468944b0c313ec00b4dec7b34f5c206e223c20e
user <user@usernoMacBook-Air.local>
2024-10-13 09:46:47 +0900

    43ch ok ProfileModels

--- userauths/admin.py
+++ userauths/admin.py
@@ -1,9 +1,14 @@
+
 from django.contrib import admin
 
-from userauths.models import User
+from userauths.models import Profile, User
 from import_export.admin import ImportExportModelAdmin
 
 class UserAdmin(ImportExportModelAdmin):
     pass
 
+class ProfileAdmin(ImportExportModelAdmin):
+    list_display = ['user']
+
 admin.site.register(User,UserAdmin)
+admin.site.register(Profile,ProfileAdmin)
--- userauths/models.py
+++ userauths/models.py
@@ -1,6 +1,9 @@
 from django.db import models
-
+from django.db.models import Model
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
+
+from core.models import Video
 
 class User(AbstractUser):
     email = models.EmailField(unique=True, null=True)
@@ -11,3 +14,21 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+class Profile(Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    saved_videos = models.ManyToManyField(Video, null=True, blank=True, related_name="saved_videos")
+
+    def __str__(self):
+        return self.user.username
+    
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+    # user.profile.save() >> destiny.profile.save()
+
+post_save.connect(create_user_profile, sender=User)
+post_save.connect(save_user_profile, sender=User)
