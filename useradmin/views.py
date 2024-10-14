f68589b0c72f39570e95e1682adac5a9087374d6
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-10-14 15:58:58 +0900

    58ch ok ?

---
+++ useradmin/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from channel.models import Channel
+from core.models import Video, Comment
+from django.contrib.auth.decorators import login_required
+
+
+@login_required
+def Studio(request):
+    user = request.user
+    channel = user.channel
+
+    channel = Channel.objects.get(user=user)
+    videos = Video.objects.filter(user=user)
+    comments = Comment.objects.filter()
+
+    context = {
+        "user":user,
+        "channel":channel,
+        "videos":videos,
+    }
+
+    return render(request, "useradmin/studio.html", context)
